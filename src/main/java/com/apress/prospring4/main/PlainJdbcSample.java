6a6854926f47f328a8c6f9b78b7ec45f27b73d5a
SK040459 <suman.khara@cerner.com>
2018-10-25 17:53:54 +0530

    using log4j

--- src/main/java/com/apress/prospring4/main/PlainJdbcSample.java
+++ src/main/java/com/apress/prospring4/main/PlainJdbcSample.java
@@ -4,19 +4,24 @@ import java.sql.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import com.apress.prospring4.dao.ContactDao;
 import com.apress.prospring4.dao.ContactDaoJDBCImpl;
 import com.apress.prospring4.model.Contact;
 
 public class PlainJdbcSample {
+	
+	private static final Logger logger = LogManager.getLogger(PlainJdbcSample.class);
 
 	private static ContactDao contactDao = new ContactDaoJDBCImpl();
 	
 	public static void main(String[] args) {
-		System.out.println("Listing initial contact data: ");
+		logger.info("Listing initial contact data: ");
 		listAllContacts();
 		
-		System.out.println("Insert a new contact:");
+		logger.info("Insert a new contact:");
 		Contact contact = new Contact();
 		contact.setFirstName("Jacky");
 		contact.setLastName("Chan");
@@ -24,14 +29,14 @@ public class PlainJdbcSample {
 		
 		contactDao.insert(contact);
 		
-		System.out.println("Listing contact data after new contact created: ");
+		logger.info("Listing contact data after new contact created: ");
 		listAllContacts();
 		
-		System.out.println("Deleting the previous created contact: ");
+		logger.info("Deleting the previous created contact: ");
 		
 		contactDao.delete(contact.getId());
 		
-		System.out.println("Listing contact data after new contact deleted: ");
+		logger.info("Listing contact data after new contact deleted: ");
 		listAllContacts();
 	}
 
@@ -39,7 +44,7 @@ public class PlainJdbcSample {
 		List<Contact> contacts = contactDao.findAll();
 		
 		for(Contact contact: contacts) {
-			System.out.println(contact);
+			logger.info(contact);
 		}
 	}
 
